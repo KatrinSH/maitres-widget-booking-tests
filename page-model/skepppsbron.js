5a653d69196119a211f796cb8856db59da5b3e08
ekaterinasugaj <shugay_k@ukr.net>
2023-01-03 18:01:19 +0200

    [FUTURE]: added test for check opened days in restaurant

--- page-model/skepppsbron.js
+++ page-model/skepppsbron.js
@@ -2,17 +2,25 @@ import { Selector } from 'testcafe';
 
 class skepppsbron {
     constructor() {
-        this.testDays = [3, 4, 5, 6, 7, 10]
+        this.testClosedDays = [3, 4, 5, 6, 10]
+        this.testOpenedDays = [7, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31]
         this.nextMonth = Selector('#app .MuiSvgIcon-root').nth(7);
         this.expText = Selector('#app h1')
+        this.expButtonHelMenu = Selector('#app button').withText('HEL MENY / FULL MENU (1195 KR)')
+        this.expButtonHalvMenu = Selector('#app button').withText('HALV MENY / HALF MENU (695 KR)')
     }
 
-    getDaySelector(index) {
-        const onlyNumberDay = new RegExp("\\b(" + this.testDays[index] + ")\\b", 'i');
+    getClosedDaySelector(index) {
+        const onlyNumberClosedDay = new RegExp("\\b(" + this.testClosedDays[index] + ")\\b", 'i');
+       
 
-        return Selector('#app .MuiPickersBasePicker-pickerView .MuiPickersDay-day:not(.MuiPickersDay-dayDisabled) p').withText(onlyNumberDay)
+        return Selector('#app .MuiPickersBasePicker-pickerView .MuiPickersDay-day:not(.MuiPickersDay-dayDisabled) p').withText(onlyNumberClosedDay)
     }
-
+    getOpenedDaySelector(index) {
+    const onlyNumberOpenedDay = new RegExp("\\b(" + this.testOpenedDays[index] + ")\\b", 'i');
+    
+    return Selector('#app .MuiPickersBasePicker-pickerView .MuiPickersDay-day:not(.MuiPickersDay-dayDisabled) p').withText(onlyNumberOpenedDay)
+    }
 }
 
 export default new skepppsbron();
--- tests/open_hours_in_restaursnt.js
+++ tests/open_hours_in_restaursnt.js
@@ -4,12 +4,22 @@ fixture`Users`
     .page`https://cloud.maitres.com/?hash=f6597937c607495f0e080ee0d25aff33`
 
 
-test('New Test', async t => {
-    for (let index = 0; index < skepppsbron.testDays.length; index++) {
+test('Check closed days', async t => {
+    for (let index = 0; index < skepppsbron.testClosedDays.length; index++) {
         await t
-            .click(skepppsbron.nextMonth)
-            .click(skepppsbron.getDaySelector(index))
+            .click(skepppsbron.getClosedDaySelector(index))
             .expect((skepppsbron.expText).textContent).eql("Tyvärr finns inga lediga bord önskad dag, kontakta oss för väntlista.")
             .navigateTo('https://cloud.maitres.com/?hash=f6597937c607495f0e080ee0d25aff33')
+            
     }
+  
 });
+test ('Check opened days ',async t=> {
+    for (let index = 0; index < skepppsbron.testOpenedDays.length; index++){
+        await t
+        .click(skepppsbron.getOpenedDaySelector(index))
+        .expect((skepppsbron.expButtonHelMenu).textContent).ok()
+        .expect((skepppsbron.expButtonHalvMenu).textContent).ok()
+        .navigateTo('https://cloud.maitres.com/?hash=f6597937c607495f0e080ee0d25aff33')
+    }
+})
