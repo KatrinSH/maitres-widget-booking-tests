e057caec3d1b9aa6f72dc21e4f0d67624e4bfa59
ekaterinasugaj <shugay_k@ukr.net>
2023-01-05 00:40:36 +0200

    UPDATE: Added url to constant and  looped test

--- constants.js
+++ constants.js
@@ -1,1 +1,8 @@
-export const HOLIDAY_DAYS = [3, 4, 5, 6, 10];
+export const HOLIDAY_DAYS = [3, 4, 5, 6, 7, 10];
+export const SKEPPSBRON_HASH = 'f6597937c607495f0e080ee0d25aff33'
+
+export const INIT_PARAMS = {
+    hash: SKEPPSBRON_HASH
+}
+
+export const URL_QUERY_PARAMS = `?${new URLSearchParams(INIT_PARAMS).toString()}`;
--- tests/open_hours_in_restaursnt.js
+++ tests/open_hours_in_restaursnt.js
@@ -1,28 +1,30 @@
+import { URL_QUERY_PARAMS } from "../constants";
 import skepppsbron from "../page-model/skepppsbron";
 
 fixture`Users`
-    .page`https://cloud.maitres.com/?hash=f6597937c607495f0e080ee0d25aff33`
+    .page`./${URL_QUERY_PARAMS}`
 
 
-test('Check closed days', async t => {
-    for (let index = 0; index < skepppsbron.testClosedDays.length; index++) {
+for (let index = 0; index < skepppsbron.testClosedDays.length; index++) {
+    test(`Check closed days: day - ${skepppsbron.testClosedDays[index]}`, async t => {
         await t
             .click(skepppsbron.getClosedDaySelector(index))
             .expect((skepppsbron.expText).textContent).eql("Tyvärr finns inga lediga bord önskad dag, kontakta oss för väntlista.")
-            .navigateTo('https://cloud.maitres.com/?hash=f6597937c607495f0e080ee0d25aff33')
-            
-    }
-  
-});
-test ('Check opened days ',async t=> {
-    for (let index = 0; index < skepppsbron.testOpenedDays.length; index++){
+            .navigateTo(`./${URL_QUERY_PARAMS}`)
+
+
+    });
+}
+
+for (let index = 0; index < skepppsbron.testOpenedDays.length; index++) {
+    test(`Check opened days: day - ${skepppsbron.testOpenedDays[index]}`, async t => {
         await t
-        .click(skepppsbron.getOpenedDaySelector(index))
-        .click(skepppsbron.ButtonHelMenu)
-        .expect((skepppsbron.expEmptyTable).textContent).ok()
-        .click(skepppsbron.backButton)
-        .click(skepppsbron.ButtonHalvMenu)
-        .expect((skepppsbron.expEmptyTable).textContent).ok()
-        .navigateTo('https://cloud.maitres.com/?hash=f6597937c607495f0e080ee0d25aff33')
-    }
-})
+            .click(skepppsbron.getOpenedDaySelector(index))
+            .click(skepppsbron.ButtonHelMenu)
+            .expect((skepppsbron.expEmptyTable).textContent).ok()
+            .click(skepppsbron.backButton)
+            .click(skepppsbron.ButtonHalvMenu)
+            .expect((skepppsbron.expEmptyTable).textContent).ok()
+            .navigateTo(`./${URL_QUERY_PARAMS}`)
+    })
+}
